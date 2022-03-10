884b7f2c4e940b203acc8b3164e231e8ec6f0b1c
Kestutis Kravcovas <KestutisKravcovas@coherentsolutions.com>
2022-03-10 15:45:24 +0200

    SquareMatrix indexer done

---
+++ NET01.2Solution/NET01.2Task/Program.cs
@@ -0,0 +1,17 @@
+﻿using NET01._2Task;
+
+try
+{
+    SquareMatrix<int> sqMatrix1 = new(3);
+    sqMatrix1[0, 0] = 1;
+    sqMatrix1[0, 1] = 2;
+    sqMatrix1[0, 2] = 3;
+    sqMatrix1[1, 4] = 4;
+    Console.WriteLine(sqMatrix1.ToString());
+    Console.WriteLine("/////////////////");
+}
+catch (ArgumentOutOfRangeException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
---
+++ NET01.2Solution/NET01.2Task/SquareMatrix.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET01._2Task
+{
+    internal class SquareMatrix<T>
+    {
+        public int Size { get; set; }
+        public T[] Values { get; set; }
+
+        public SquareMatrix(int size)
+        {
+            Size = size >= 0 ? size : throw new ArgumentOutOfRangeException(nameof(size), "Matrix size cannot be negative");
+            Values = new T[size];
+        }
+
+        public T this[int i, int j]
+        {
+            get
+            {
+                int index;
+                
+                if(i < 0 || i >= Values.Length || j < 0 || j >= Values.Length)
+                {
+                    throw new ArgumentOutOfRangeException("Invalid matrix element index");
+                }
+                else
+                {
+                     index = i * Size + j;
+                }
+
+                return Values[index];
+            }
+
+            set
+            {
+                int index;
+
+                if (i < 0 || i >= Values.Length || j < 0 || j >= Values.Length)
+                {
+                    throw new ArgumentOutOfRangeException("Invalid matrix element index");
+                }
+                else
+                {
+                    index = i * Size + j;
+                }
+
+                Values[index] = value;
+            }
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach(var cell in Values)
+            {
+                sb.Append(cell.ToString());
+            }
+            return sb.ToString();
+        }
+    }
+}
